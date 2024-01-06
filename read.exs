53fe160ea38064ad47c859774e88deebf7dd5ddd
Rijan Buddhacharya <97865921+Rijan-Budds@users.noreply.github.com>
2024-01-06 18:43:59 +0545

    Delete read.exs

--- read.exs
+++
@@ -1,8 +0,0 @@
- def read_notes() do
-    case File.read("note.txt") do
-    {:ok, content} ->  IO.puts("Your notes:\n#{content}")
-    {:error, reason} -> IO.puts("Error reading file: #{reason}")
-    end
-    end
-
-    Notes.read_notes()
